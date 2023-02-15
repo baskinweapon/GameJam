d36d87347abbe956a0e94e5a5247bd598d092e47
AlexBaskin <solohinalex1@gmail.com>
2023-02-15 19:09:28 +0200

    add general

---
+++ Assets/Scripts/General/InputSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputSystem : MonoBehaviour {
+    public DefaultInputActions actions;
+    
+    void OnEnable() {
+        actions.Player.Fire.performed += Fire;
+        actions.Player.Move.performed += Move;
+    }
+
+    private void Move(InputAction.CallbackContext ctx) => Debug.Log(ctx.ReadValue<Vector2>());
+
+        private void Fire(InputAction.CallbackContext ctx) => Debug.Log("Fire");
+
+    private void OnDisable() {
+        actions.Player.Fire.performed -= Fire;
+        actions.Player.Move.performed -= Move;
+    }
+}
